b2297a278cc6f8229d59e0779dc1c0743697f9e4
Spenza <spenzayt@gmail.com>
2025-02-14 17:14:10 +0100

    Ajout de Squelette_Projet_IA

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <SFML/Graphics.hpp>
+#include "entities/Player.hpp"
+#include "entities/Enemy.hpp"
+#include "map/Grid.hpp"
+#include <vector>
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "test");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Jeu SFML - IA Ennemis");
+    window.setFramerateLimit(60);
+
+    Player player(200, 400);
+    std::vector<Enemy> enemies = { Enemy(100, 100), Enemy(700, 100) };
+    Grid grid;
+    grid.loadFromFile("src/map/map.txt");
+
+    sf::Clock clock;
 
     while (window.isOpen()) {
+        sf::Time dt = clock.restart();
+        float deltaTime = dt.asSeconds();
+
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+        player.update(deltaTime, grid);
+        for (auto& enemy : enemies) {
+            enemy.update(deltaTime, grid);
+        }
+
         window.clear();
+        grid.draw(window);
+        window.draw(player.shape);
+        for (const auto& enemy : enemies)
+            window.draw(enemy.shape);
         window.display();
     }
-
     return 0;
 }
+
